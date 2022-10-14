51e5866a437669fe8e8d7d389609a41e2f3c8e59
nazarPtyt <ockap90@libero.it>
2022-10-14 13:54:51 +0200

    added Footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Main from "./Main/Main";
 import ContactMe from "./ContactMe/ContactMe";
+import Footer from "./Footer/Footer";
 
 function App() {
     return (
         <div>
             {/*<Main/>*/}
-            <ContactMe/>
+            {/*<ContactMe/>*/}
+            <Footer/>
         </div>
     );
 }
---
+++ src/Footer/Footer.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import s from './Footer.module.css';
+import facebook from './icons/facebook.svg';
+import github from './icons/github.svg';
+import instagram from './icons/instagram.svg';
+import linkedin from './icons/linkedin.svg';
+import arrowUp from './icons/arrow-up.svg'
+
+
+
+const Footer = () => {
+    return (
+        <footer className={s.footerWrap}>
+            <div className={'container'}>
+                <nav className={s.nav}>
+                    <a href="#"><img className={s.icon} src={facebook} alt="facebook-icon"/></a>
+                    <a href="#"><img className={s.icon} src={github} alt="github-icon"/></a>
+                    <a href="#"><img className={s.icon} src={instagram} alt="instagram-icon"/></a>
+                    <a href="#"><img className={s.icon} src={linkedin} alt="linkedin-icon"/></a>
+                </nav>
+                <p className={s.copyright}>Copyright © <time dateTime="2022">2022</time> All rights reserved by <span className={s.myName}>Nazar Prytuliak</span>. Built with love in Italy :)</p>
+                <a href="#" className={s.arrowUp}><img className={s.icon} src={arrowUp} alt="arrow-up-icon"/></a>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
