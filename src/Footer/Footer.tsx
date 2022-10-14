a65557fc688dc7feea4b90e0817229b11e016b44
nazarPtyt <ockap90@libero.it>
2022-10-14 15:05:37 +0200

    Fixed problem wth arrowUp

--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -7,7 +7,6 @@ import linkedin from './icons/linkedin.svg';
 import arrowUp from './icons/arrow-up.svg'
 
 
-
 const Footer = () => {
     return (
         <footer className={s.footerWrap}>
@@ -18,8 +17,18 @@ const Footer = () => {
                     <a href="#"><img className={s.icon} src={instagram} alt="instagram-icon"/></a>
                     <a href="#"><img className={s.icon} src={linkedin} alt="linkedin-icon"/></a>
                 </nav>
-                <p className={s.copyright}>Copyright © <time dateTime="2022">2022</time> All rights reserved by <span className={s.myName}>Nazar Prytuliak</span>. Built with love in Italy :)</p>
-                <a href="#" className={s.arrowUp}><img className={s.icon} src={arrowUp} alt="arrow-up-icon"/></a>
+                <p className={s.copyright}>Copyright © <time dateTime="2022">2022</time> All rights reserved by
+                    <span className={s.myName}>Nazar Prytuliak</span>. Built with love in Italy :)
+                </p>
+                {/*<a href="#" className={s.arrowUp}><img className={s.icon} src={arrowUp} alt="arrow-up-icon"/></a>*/}
+                <a href="#" className={s.arrowLink}>
+                    <div className={s.arrowUPs}>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                </a>
+
             </div>
         </footer>
     );
